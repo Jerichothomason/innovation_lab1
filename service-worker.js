3f4732ebe63c067bc870201d0ce63540d60c86b0
Jericho Thomason <jerichothomason@gmail.com>
2020-09-21 18:57:49 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jerichothomason/innovation_lab1.git/precache-manifest.b4a384ecf987c8f311eecbbb3d4b0354.js"
+  "/Jerichothomason/innovation_lab1/precache-manifest.c4fe8da9f2bcebdabb3e76a009a6d77f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jerichothomason/innovation_lab1.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jerichothomason/innovation_lab1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
